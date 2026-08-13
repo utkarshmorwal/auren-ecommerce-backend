c377020aaa369191531baee22d2f78910f4b6c2a
Utkarsh Morwal <137719110+utkarshmorwal@users.noreply.github.com>
2026-08-13 14:50:09 +0530

    Update allowed origins for WebSocket endpoint

--- src/main/java/com/ecommerce/config/WebSocketConfig.java
+++ src/main/java/com/ecommerce/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
     	registry.addEndpoint("/ws")
-        .setAllowedOrigins("http://localhost:5173", "https://veylo-ecommerce-frontend.vercel.app")
+        .setAllowedOrigins("http://localhost:5173", "https://auren-clothing-fashion.vercel.app")
         .withSockJS();
     }
 }
