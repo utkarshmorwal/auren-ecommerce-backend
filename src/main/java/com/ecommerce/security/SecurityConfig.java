11de6a6a82f8785193580f2ceed0fe27db2ad702
Utkarsh Morwal <137719110+utkarshmorwal@users.noreply.github.com>
2026-08-13 14:02:50 +0530

    Update allowed origins in CORS configuration

--- src/main/java/com/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/ecommerce/security/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173",  "https://auren-ecommerce-frontend.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173",  ""https://auren-clothing-fashion.vercel.app""));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
